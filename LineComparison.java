f5b032658928d74f7ace3b5f962b40d42e4a3bbb
1riddhi <prajapatiriddhi074@gmail.com>
2023-12-27 06:29:46 +0530

    Implemented UC-2: Check two lines are equal or not

--- LineComparison.java
+++ LineComparison.java
@@ -1,11 +1,25 @@
 import java.util.Scanner;
 
-public class LineComarison {
+public class LineComparison {
     public static void main(String[] args) {
         System.out.println("Welcome to Line Comparison Program.");
 
+        Line line1=getLine();
+        Line line2=getLine();
+
+        System.out.println("Length of Line-1: "+line1.calculateLength());
+        System.out.println("Length of Line-2: "+line2.calculateLength());
+
+        if(line1.equals(line2)) System.out.println("Two lines are equal");
+        else System.out.println("Two lines are not equal");;
+
+    }
+
+    public static Line getLine(){
+
         Scanner scanner = new Scanner(System.in);
-        System.out.print("Enter x-1");
+
+        System.out.print("Enter x-1: ");
         double x1 = scanner.nextDouble();
         System.out.print("Enter y-1: ");
         double y1 = scanner.nextDouble();
@@ -19,13 +33,13 @@ public class LineComarison {
 
         Line line=new Line(p1,p2);
 
-        System.out.println("Length of Line: "+line.calculateLength());
+        return line;
     }
 }
 
-public class Point{
+ class Point{
     double x,y;
-    public Point(x,y){
+    public Point(double x,double y){
         this.x=x;
         this.y=y;
     }
@@ -46,5 +60,9 @@ class Line {
         double y2 = point2.y;
 
         return Math.sqrt(Math.pow(x2 - x1, 2) + Math.pow(y2 - y1, 2));
+    }
+
+    public static boolean equals(Line line1, Line line2){
+        return line1.point1.x==line2.point1.x && line1.point1.y==line2.point1.y &&line1.point2.x==line2.point2.x && line1.point2.y==line2.point2.y;
     }
 }
